dd3457f129d45ebec9efe8b587dacd4999af54dd
niwa-niwa <urban.hang274@gmail.com>
2021-11-07 23:55:01 +0900

    made StartPage.js and refactored

--- src/TestAuth.js
+++ src/TestAuth.js
@@ -1,13 +1,12 @@
-import React from "react"
+import React from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
 import {
   FirebaseAuthProvider,
   FirebaseAuthConsumer,
   IfFirebaseAuthed,
-  IfFirebaseAuthedAnd
+  IfFirebaseAuthedAnd,
 } from "@react-firebase/auth";
-
 
 const firebase_config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,24 +15,22 @@ const firebase_config = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-}
+};
 
 const email_login = {
   email: process.env.REACT_EMAIL,
-  password: process.env.REACT_PASSWORD
-}
+  password: process.env.REACT_PASSWORD,
+};
 
-
-const TestAuth = () => {
+export default function TestAuth() {
   return (
     <FirebaseAuthProvider {...firebase_config} firebase={firebase}>
       <div>
-
         {/* Google Sign in */}
         <button
           onClick={() => {
-            const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
-            firebase.auth().signInWithPopup(googleAuthProvider)
+            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(googleAuthProvider);
           }}
         >
           Sign In with Google
@@ -43,7 +40,7 @@ const TestAuth = () => {
         <button
           data-testid="signin-anon"
           onClick={() => {
-            firebase.auth().signInAnonymously()
+            firebase.auth().signInAnonymously();
           }}
         >
           Sign In Anonymously
@@ -54,10 +51,15 @@ const TestAuth = () => {
 
         {/* E-Mail Sign up */}
         <button
-          onClick={async () =>  {
-            const res = await firebase.auth().createUserWithEmailAndPassword(email_login.email, email_login.password)
-            res.user.sendEmailVerification()
-            console.log(res)
+          onClick={async () => {
+            const res = await firebase
+              .auth()
+              .createUserWithEmailAndPassword(
+                email_login.email,
+                email_login.password
+              );
+            res.user.sendEmailVerification();
+            console.log(res);
           }}
         >
           Sign In with Mail
@@ -65,8 +67,15 @@ const TestAuth = () => {
 
         {/* E-Mail Sign in */}
         <button
-          onClick={() => {firebase.auth().signInWithEmailAndPassword(email_login.email, email_login.password)}}
-          >
+          onClick={() => {
+            firebase
+              .auth()
+              .signInWithEmailAndPassword(
+                email_login.email,
+                email_login.password
+              );
+          }}
+        >
           Log In with Mail
         </button>
 
@@ -76,7 +85,7 @@ const TestAuth = () => {
         {/* Sign out */}
         <button
           onClick={() => {
-            firebase.auth().signOut()
+            firebase.auth().signOut();
           }}
         >
           Sign Out
@@ -88,52 +97,45 @@ const TestAuth = () => {
               <pre style={{ height: 300, overflow: "auto" }}>
                 {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
               </pre>
-            )
+            );
           }}
         </FirebaseAuthConsumer>
 
         <div>
-
           <IfFirebaseAuthed>
-            {
-            
-              () => {
-                // To confirm verified Email
-                const user = firebase.auth().currentUser
-                if(user.emailVerified){
-                  return <div>You are authenticated</div>
-                }else{
-                  return (
-                    <div>
-                      <div style={{ color: "#ff0000" }}>
-                        You have to verify email
-                      </div>
-                      <button onClick={() => {
-                        user.sendEmailVerification()
-                      }}>
-                        Please Send Email Again
-                      </button>
+            {() => {
+              // To confirm verified Email
+              const user = firebase.auth().currentUser;
+              if (user.emailVerified) {
+                return <div>You are authenticated</div>;
+              } else {
+                return (
+                  <div>
+                    <div style={{ color: "#ff0000" }}>
+                      You have to verify email
                     </div>
-                  )
-                }
+                    <button
+                      onClick={() => {
+                        user.sendEmailVerification();
+                      }}
+                    >
+                      Please Send Email Again
+                    </button>
+                  </div>
+                );
               }
-            
-            }
+            }}
           </IfFirebaseAuthed>
 
           <IfFirebaseAuthedAnd
             filter={({ providerId }) => providerId !== "anonymous"}
           >
             {({ providerId }) => {
-              return <div>You are authenticated with {providerId}</div>
+              return <div>You are authenticated with {providerId}</div>;
             }}
           </IfFirebaseAuthedAnd>
-
         </div>
-        
       </div>
     </FirebaseAuthProvider>
-
-  )
+  );
 }
-export default TestAuth
